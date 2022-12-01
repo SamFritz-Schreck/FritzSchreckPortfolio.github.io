eb922b8724bc9f026b675cf075e0f29c4e2cd75e
SamFritz-Schreck <112218167+SamFritz-Schreck@users.noreply.github.com>
2022-12-01 10:38:51 -0800

    Update date.sh

--- date.sh
+++ date.sh
@@ -3,7 +3,7 @@
 git_last_commit_date="$(git log -1 --format=%cd)"
 echo "Git last commit date: $git_last_commit_date"
 
-html_files="$(find . -type f -name "*.html")"
+md_files="$(find . -type f -name "*.md")"
 
 for file in $html_files; do 
   echo "Inserting date into: $file" 
